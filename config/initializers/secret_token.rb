3623a681e52b93384bb06f2f44e46c606b9f588c
David Madrigal <mc.david.madrigal@gmail.com>
2012-09-13 18:41:56 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MightBuy::Application.config.secret_token = 'd3abe49562a57a2d7d74df5ee598ba578c2059cc5d099ac39e33d09e9131f78204137f325b5553e621e591d717bc324623f1422dc9c5289150ed5cdd56ebd677'
