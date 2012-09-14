610617f007c0d6f482a53a2ebc1090274d6c6a5e
David Madrigal <mc.david.madrigal@gmail.com>
2012-09-14 10:40:12 -0500

    user devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 MightBuy::Application.routes.draw do
+  devise_for :users
+
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+  
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
